24c4b91bb5d4dbeb8f776f49362957090b2a4f56
satoruwww <margjim353@gmail.com>
2026-01-21 14:38:42 +0000

    Added the new feature

--- calculator.py
+++ calculator.py
@@ -9,3 +9,28 @@ display_var.set(expression)
 def clear():
     global expression
     expression= "" display_var.set("")
+
+    def erase():
+    global expression
+    expression = expression[:-1]
+    display_var.set(expression)
+
+def calculate():
+    global expression
+    try:
+        result = str(eval(expression))
+        display_var.set(result)
+        expression = result
+    except:
+        display_var.set("Error")
+        expression = ""
+
+
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("300x500")
+root.configure(bg="#ffffff")
+root.resizable(False, False)
+
+expression = ""
+display_var = tk.StringVar()
