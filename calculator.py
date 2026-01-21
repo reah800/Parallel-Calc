02d12df957f1cb0f42fd94df737f760c1cd093fe
Kai-Zen23 <jaymarklabial09@gmail.comgit>
2026-01-21 14:12:47 +0000

    add functions

--- calculator.py
+++ calculator.py
@@ -1,2 +1,11 @@
 import tkinter as tk
 
+def press(key):
+    global expression
+    expression += str(key)
+
+display_var.set(expression)
+
+def clear():
+    global expression
+    expression= "" display_var.set("")
