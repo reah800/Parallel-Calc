e31f0cbbea097cb30d02e91ed8d3d660cbe800ee
Karkar <carmichaeldamalan31@github.com>
2026-02-08 23:06:09 +0800

    Added what feature

--- secondactivity.py
+++ secondactivity.py
@@ -14,3 +14,23 @@ def thread_worker(grades, queue):
 def process_worker(grades, queue):
     gwa = compute_gwa(grades)
     queue.put(gwa)
+
+def run_multithreading(grades, tree, progress):
+    start = time.time()
+    queue = Queue()
+    threads = []
+
+    for grade in grades:
+        t = threading.Thread(target=thread_worker, args=([grade], queue))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    results = []
+    while not queue.empty():
+        results.append(queue.get())
+
+    end = time.time()
+    overall_gwa = compute_gwa(grades)
